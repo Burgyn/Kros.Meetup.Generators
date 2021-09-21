8fe9939ff724cc092506fea8f97de500dbdc9342
Milan Martiniak <mino.martiniak@gmail.com>
2021-09-21 06:20:47 +0200

    Init

---
+++ Kros.Meetup/ClassModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kros.Meetup
+{
+    internal class ClassModel
+    {
+        public string Namespace { get; set; }
+
+        public string Name { get; set; }
+
+        public string Modifier { get; set; }
+
+        public string[] Properties { get; set; }
+    }
+}
